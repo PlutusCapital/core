18503b378323f486e9d6c06b6da984f1479b37e5
Shanu Goyanka <sgoyanka@deqode.com>
2019-12-02 19:40:42 +0530

    Added dockerfile for running multiple nodes

---
+++ entrypoint.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+touch /pivx.conf
+
+x=$REMOTES
+OLD_IFS=$IFS    # save internal field separator
+IFS=","         # set it to '+'
+set -- $x       # make the result positional parameters
+IFS=$OLD_IFS    # restore IFS
+for i in "$@"; do # access each element of array
+    echo "addnode = $i" >> /pivx.conf
+done
+
+echo "gen = 1" >> /pivx.conf
+
+cd /app && ./pivxd -conf=pivx.conf
