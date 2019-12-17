bc281ae42fe2f03dbeac4895e7d8e9f68aee9071
Shivam Bohare <shivambohare@gmail.com>
2019-12-17 15:56:49 +0530

    added older magic bytes as comments to be used as a last resort

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -151,10 +151,17 @@ public:
          * The characters are rarely used upper ASCII, not valid as UTF-8, and produce
          * a large 4-byte int at any alignment.
          */
+
+        /*-----17/12/2019 changed the magic bytes by using UNIX's Random library -----*/
         pchMessageStart[0] = 0xc6;
         pchMessageStart[1] = 0x88;
         pchMessageStart[2] = 0xd6;
         pchMessageStart[3] = 0x25;
+        // pchMessageStart[0] = 0xC6;
+        // pchMessageStart[1] = 0xc4;
+        // pchMessageStart[2] = 0xfd;
+        // pchMessageStart[3] = 0xe9;
+
         vAlertPubKey = ParseHex("0000098d3ba6ba6e7423fa5cbd6a89e0a9a5348f88d332b44a5cb1a8b7ed2c1eaa335fc8dc4f012cb8241cc0bdafd6ca70c5f5448916e4e6f511bcd746ed57dc50");
         nDefaultPort = 51472;
         bnProofOfWorkLimit = ~uint256(0) >> 15; // PIVX starting difficulty is 1 / 2^12
