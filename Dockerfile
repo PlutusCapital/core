3845cba5d05db0fa7ff8b55597088e28748e7f9a
Shanu Goyanka <sgoyanka@deqode.com>
2019-12-22 18:12:00 +0530

    Added network details to dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,26 @@
-FROM phusion/baseimage:0.11
+FROM ubuntu:16.04
 
 ENV LANG=en_US.UTF-8
 
 RUN apt-get update -y
 
+RUN apt-get install software-properties-common -y
+
+RUN add-apt-repository ppa:bitcoin/bitcoin
+
+RUN apt-get update -y
+
+RUN apt-get install libboost-system1.58.0 libboost-filesystem1.58.0 libboost-program-options1.58.0 libboost-thread1.58.0 libboost-chrono1.58.0 -y
+
+RUN apt-get install libdb4.8-dev libdb4.8++-dev libgmp-dev libminiupnpc-dev libevent-dev libgmp-dev libzmq3-dev -y
+
 WORKDIR /app
 
-ADD src/plutusd /app
-ADD src/plutus-cli /app
+ADD ./src/pivxd /app
+ADD ./src/pivx-cli /app
 
-ADD entrypoint.sh /app
+ADD ./entrypoint.sh /app
 
 RUN chmod 777 /app/entrypoint.sh
-RUN chmod 777 /app/plutusd
-RUN chmod 777 /app/plutus-cli
+RUN chmod 777 /app/pivxd
+RUN chmod 777 /app/pivx-cli
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,23 @@
 #!/bin/sh
 
-touch /plutus.conf
+touch ./plutus.conf
 
+mkdir .plutus
 x=$REMOTES
 OLD_IFS=$IFS    # save internal field separator
 IFS=","         # set it to '+'
 set -- $x       # make the result positional parameters
 IFS=$OLD_IFS    # restore IFS
 for i in "$@"; do # access each element of array
-    echo "addnode = $i" >> /plutus.conf
+   echo "addnode = $i" >> ./plutus.conf
 done
 
-echo "port = 11011" >> /plutus.conf
-echo "keypool = 1" >> /plutus.conf
-echo "gen = 1" >> /plutus.conf
+echo "keypool=0.01" >> ./plutus.conf
+echo "rpcuser=rpcuser" >> ./plutus.conf
+echo "rpcpassword=111111" >> ./plutus.conf
+echo "rpcallowip=10.1.1.34/255.255.255.0" >> ./plutus.conf
+echo "rpcallowip=0.0.0.0/0" >> ./plutus.conf
+echo "rpcbind=0.0.0.0" >> ./plutus.conf
 
-# check="true"
-# if [$GEN == $check]
-then
-    echo "gen = 1" >> /plutus.conf
-fi
-
-./app/plutusd -datadir=/app -printtoconsole
+mv ./plutus.conf ./.plutus
+./plutusd -datadir=./.plutus -printtoconsole
